cae5d428140a21900ddf3188ecf8b2b9b992d212
Sterling <d3rpz1lla@gmail.com>
2019-02-16 16:25:50 -0800

    add comments create end point

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,14 @@
 class CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    comment = Comment.new create_params
+    if comment.save
+      render json: comment.serialize
+    else
+      render json: { error: 'bad-request' }, status: 400
+    end
+  end
 
   def index
     render json: Comment.where(
@@ -8,6 +18,14 @@ class CommentsController < ApplicationController
   end
 
   private
+  def create_params
+    strong_param = params.require(:comment).permit(
+      :message, :commentable_id, :commentable_type
+    )
+    strong_param[:commentable_type] = strong_param[:commentable_type]&.capitalize
+    strong_param.merge(user_id: current_user.id)
+  end
+
   def index_params
     params.permit(:commentable_type, :commentable_id)
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,14 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_many   :comments,    as: :commentable
+
+  def serialize
+    {
+      id: id,
+      message: message,
+      commentable_id: commentable_id,
+      commentable_type: commentable_type,
+      author: user
+    }
+  end
 end
