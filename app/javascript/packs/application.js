da98412ccf89511748d3bc04af4f0898b73ef267
Sterling <d3rpz1lla@gmail.com>
2019-02-16 16:51:28 -0800

    setup pack entry point

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,18 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+const PostsIndex = props => (
+  <h1>Post Index</h1>
+)
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <PostsIndex />,
+    document.body.appendChild(document.createElement('div'))
+  )
+})
+
